84bedeed6f1efb474bee76997ca6c270c3cda46d
shiraji <isogai.shiraji@gmail.com>
2019-01-17 11:30:18 +0900

    Add document

--- src/main/kotlin/com/ullink/slack/simpleslackapi/SlackSession.kt
+++ src/main/kotlin/com/ullink/slack/simpleslackapi/SlackSession.kt
@@ -1,5 +1,25 @@
 package com.ullink.slack.simpleslackapi
 
+/**
+ * Kotlin friendly syntax for connect/disconnect SlackSession
+ *
+ * Normally, SlackSession needs to call connect/disconnect like this
+ * ```
+ * session.connect()
+ * val channel = session.findChannelByName("general")
+ * session.sendMessage(channel, "hi im a bot" )
+ * session.disconnect()
+ * ```
+ * Now, this method helps us write like this
+ *
+ * ```
+ * session.connect {
+ *     val channel = it.findChannelByName("general")
+ *     it.sendMessage(channel, "hi im a bot" )
+ * }
+ * ```
+ *
+ */
 fun <T> SlackSession.connect(block: (SlackSession) -> T): T {
     try {
         if (!isConnected) {
